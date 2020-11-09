d2cc454b2084ce6c0a7baa1b14c4ccd42a9d8cf3
Hugo Ribeiro <hugolerib@gmail.com>
2020-11-08 23:30:58 -0300

    Adicionando métodos de remoção

--- src/boundary/Tela.java
+++ src/boundary/Tela.java
@@ -9,6 +9,7 @@ public class Tela {
 
 	public static void main(String[] args) {
 		int opc = 0;
+		String inscrito_removido;
 
 		InscritoController metodo = new InscritoController();
 		ListaMetodos lista = new ListaMetodos();
@@ -19,19 +20,57 @@ public class Tela {
 					"Digite a opção desejada:\n"
 					+ "1 - Cadastro de Inscrito\n"
 					+ "2 - Mostra\n"
+					+ "3 - Remover o último inscrito\n"
+					+ "4 - Remover um inscrito baseado no cpf\n"
 					+ "9 - Fim"));
 
 			switch (opc) {
 			case 1:
 				//Coloquei o adiciona final aqui, porque o inicio na classe "ListaMetodos" estava perdendo os dados do "novo" quando saia do método
-				lista.adicionaFinal(metodo.cadastro());
+				try {
+					lista.adicionaFinal(metodo.cadastro());
+				}catch(Exception e){
+					JOptionPane.showMessageDialog(null, "Erro! Não foi possível inserir algum dado\n"+e);
+				}
+				
 				break;
 
 			case 2:
 				//Aqui deixei uma opção de mostra lista apenas para ir testando se funciona
-				String mostra = lista.percorre();
-				JOptionPane.showMessageDialog(null, mostra);
+				if (lista.vazia()) {
+					JOptionPane.showMessageDialog(null, "A lista está vazia!");
+				}
+				else {
+					String mostra = lista.percorre();
+					JOptionPane.showMessageDialog(null, mostra);
+				}
 				break;
+				
+			case 3:
+				if (lista.vazia()) {
+					JOptionPane.showMessageDialog(null, "A lista está vazia!");
+					break;
+				}
+				inscrito_removido = lista.remove_final();
+				JOptionPane.showInternalMessageDialog(null, inscrito_removido);
+				break;
+				
+				
+			case 4:
+				if (lista.vazia()) {
+					JOptionPane.showMessageDialog(null, "A lista está vazia!");
+					break;
+				}
+				String cpf = JOptionPane.showInputDialog("Insira aqui o cpf do inscrito que deseja remover: ");
+				inscrito_removido = lista.remove_cpf(cpf);
+				if (!inscrito_removido .equals("")) {
+					JOptionPane.showInternalMessageDialog(null, inscrito_removido);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "O cpf "+cpf+" não consta na lista.");
+				}
+				break;
+
 				
 			case 9:
 				JOptionPane.showMessageDialog(null, "Fim");
